7067746dd21de6d65f96cc6a9e4d4871856735f6
Jéssica Novo <50515278+JessicaNovo@users.noreply.github.com>
2024-11-18 12:14:17 +0000

    Improve poem display

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 function generatePoem(event) {
   event.preventDefault();
+  
+  let poemContainer = document.querySelector(".poem-container");
+  poemContainer.style.display = "block";
 
   new Typewriter("#poem", {
     strings: "This is a poem",
     autoStart: true,
-    delay: 1,
+    delay: 3,
     cursor: "",
   });
 }
